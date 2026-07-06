7bd730beeb82e4be3e33290045ade33888d29a3f
Vlada Labza <vlada.labza@gmail.com>
2026-07-06 20:02:05 +0300

    jj

--- frontend/src/Accordion/Accordion.jsx
+++ frontend/src/Accordion/Accordion.jsx
@@ -1,6 +1,6 @@
 
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { ChevronDown, ChevronUp } from "lucide-react";
 
 const Accordion = ({
