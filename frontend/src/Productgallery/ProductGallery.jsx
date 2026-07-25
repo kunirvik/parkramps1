34668ae4d2ab116666152f29367a04f781886a2d
Vlada Labza <vlada.labza@gmail.com>
2026-07-25 21:14:09 +0300

    jj

--- frontend/src/Productgallery/ProductGallery.jsx
+++ frontend/src/Productgallery/ProductGallery.jsx
@@ -1122,7 +1122,8 @@ const handleStageClick = useCallback(() => {
 }} 
         ><div className="order-2 lg:order-2">
           <Swiper
-            className="custom-swiper  h-[clamp(250px,65vw,800px)]"
+          className="custom-swiper h-[clamp(250px,calc(100px+19.5vw),600px)]"
+            // className="custom-swiper h-[250px] sm:h-[300px] md:h-[350px]"
             modules={[Pagination, Mousewheel, Thumbs]}
             pagination={{ clickable: true, el: ".custom-swiper-pagination" }}
             mousewheel={true}
