3783eba44598afba935ddccd8fe54855ff07104d
Vlada Labza <vlada.labza@gmail.com>
2026-09-08 16:05:29 +0300

    jj

--- frontend/src/MenuPage/MenuPage.jsx
+++ frontend/src/MenuPage/MenuPage.jsx
@@ -283,7 +283,7 @@ export default function MenuPage() {
         <Hero3D
           modelUrl={activeModelUrl}
           media={background}
-          restRotationY={isExploring ? 0 : (65 * Math.PI) / 180}
+          restRotationY={isExploring ? 0 : (62 * Math.PI) / 180}
         />
 
         <AnimatePresence>
